59634f3109e7e9caf314f7df92e54c064721e780
Champ <droped17@gmail.com>
2024-03-06 11:31:32 +0700

    refactor: add authOption.ts

---
+++ app/api/auth/[...nextauth]/authOption.ts
@@ -0,0 +1,78 @@
+// nextauth-config.ts
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GitHubProvider from "next-auth/providers/github";
+import bcrypt from "bcrypt";
+import User from "@/app/models/Users";
+
+const authOptions: AuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+
+      profile(profile) {
+        console.log(`Profile Github: `, profile);
+        let userRole = "Github User";
+        console.log(`USER ROLE======> `, userRole);
+
+        return {
+          ...profile,
+          role: userRole,
+        };
+      },
+    }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "username",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "password",
+        },
+      },
+
+      async authorize(credentials, req) {
+        try {
+          if (!credentials) return null;
+          // OK found
+          const foundUser = await User.findOne({
+            username: credentials?.username,
+          }).exec();
+          console.log(`Found User: `, foundUser);
+
+          if (foundUser) {
+            console.log("User Exits");
+            const match = await bcrypt.compare(
+              credentials?.password as string,
+              foundUser.password
+            );
+
+            if (match) {
+              console.log("Good Pass");
+              delete foundUser.password;
+              console.log(`CREDENTIAL:===>`, { credentials });
+              return {
+                name: foundUser.username,
+              };
+            }
+          }
+        } catch (error) {
+          console.log(error);
+        }
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
+
+export default authOptions;
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,84 +1,86 @@
 import NextAuth from "next-auth/next";
 // import { options } from "./options";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
-import User from "@/app/models/Users";
-import connect from "../../../utils/db";
-import GitHubProvider from "next-auth/providers/github";
-import { AuthOptions } from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import bcrypt from "bcrypt";
+// import User from "@/app/models/Users";
+// import connect from "../../../utils/db";
+// import GitHubProvider from "next-auth/providers/github";
+// import { AuthOptions } from "next-auth";
 
-export const authOptions: AuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+import authOptions from "./authOption";
 
-      profile(profile) {
-        console.log(`Profile Github: `, profile);
-        let userRole = "Github User";
-        console.log(`USER ROLE======> `, userRole);
+// export const authOptions: AuthOptions = {
+//   providers: [
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_ID as string,
+//       clientSecret: process.env.GITHUB_SECRET as string,
 
-        return {
-          ...profile,
-          role: userRole,
-        };
-      },
-    }),
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        username: {
-          label: "Username",
-          type: "text",
-          placeholder: "username",
-        },
-        password: {
-          label: "Password:",
-          type: "password",
-          placeholder: "password",
-        },
-      },
+//       profile(profile) {
+//         console.log(`Profile Github: `, profile);
+//         let userRole = "Github User";
+//         console.log(`USER ROLE======> `, userRole);
 
-      async authorize(credentials, req) {
-        try {
-          if (!credentials) return null;
-          // OK found
-          const foundUser = await User.findOne({
-            username: credentials?.username,
-          }).exec();
-          console.log(`Found User: `, foundUser);
+//         return {
+//           ...profile,
+//           role: userRole,
+//         };
+//       },
+//     }),
+//     CredentialsProvider({
+//       id: "credentials",
+//       name: "Credentials",
+//       credentials: {
+//         username: {
+//           label: "Username",
+//           type: "text",
+//           placeholder: "username",
+//         },
+//         password: {
+//           label: "Password:",
+//           type: "password",
+//           placeholder: "password",
+//         },
+//       },
 
-          if (foundUser) {
-            console.log("User Exits");
-            const match = await bcrypt.compare(
-              credentials?.password as string,
-              foundUser.password
-            );
+//       async authorize(credentials, req) {
+//         try {
+//           if (!credentials) return null;
+//           // OK found
+//           const foundUser = await User.findOne({
+//             username: credentials?.username,
+//           }).exec();
+//           console.log(`Found User: `, foundUser);
 
-            if (match) {
-              console.log("Good Pass");
-              delete foundUser.password;
-              console.log(`CREDENTIAL:===>`,{ credentials });
-              // return {
-              //   name: foundUser.username
-              // };
-              return foundUser
+//           if (foundUser) {
+//             console.log("User Exits");
+//             const match = await bcrypt.compare(
+//               credentials?.password as string,
+//               foundUser.password
+//             );
+
+//             if (match) {
+//               console.log("Good Pass");
+//               delete foundUser.password;
+//               console.log(`CREDENTIAL:===>`,{ credentials });
+//               // return {
+//               //   name: foundUser.username
+//               // };
+//               // return foundUser
               
-            }
-          }
-        } catch (error) {
-          console.log(error);
-        }
-        return null;
-      },
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
+//             }
+//           }
+//         } catch (error) {
+//           console.log(error);
+//         }
+//         return null;
+//       },
+//     }),
+//   ],
+//   session: {
+//     strategy: "jwt",
+//   },
 
-};
+// };
 
 const handler = NextAuth(authOptions);
 
