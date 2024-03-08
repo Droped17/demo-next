5abd33ed82b351e98dcf90fc011bf20a8c9b8adf
Champ <droped17@gmail.com>
2024-03-08 10:21:33 +0700

    fix: mongoose2

--- app/utils/db.js
+++ app/utils/db.js
@@ -1,20 +1,44 @@
+// require('dotenv').config();
+
+// import mongoose from "mongoose";
+
+// const MONGODB_URI = 'mongodb+srv://admin:Ean3vPRCIdNrm0X6@cluster0.inamacw.mongodb.net/AppDB';
+
+// const connect = async () => {
+//   if (mongoose.connections[0].readyState) return;
+
+//   try {
+//     await mongoose.connect(MONGODB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("Mongo Connection successfully established.");
+//   } catch (error) {
+//     throw new Error("Error connecting to Mongoose");
+//   }
+// };
+
+// export default connect;
+
 require('dotenv').config();
+import mongoose from 'mongoose';
 
-import mongoose from "mongoose";
+const MONGODB_URI = process.env.MONGODB_URI;
 
-const MONGODB_URI = 'mongodb+srv://admin:Ean3vPRCIdNrm0X6@cluster0.inamacw.mongodb.net/AppDB';
+if (!MONGODB_URI) {
+  throw new Error('MongoDB URI is not defined in the environment variables.');
+}
 
 const connect = async () => {
-  if (mongoose.connections[0].readyState) return;
-
   try {
     await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("Mongo Connection successfully established.");
+    console.log('Mongo Connection successfully established.');
   } catch (error) {
-    throw new Error("Error connecting to Mongoose");
+    console.error('Error connecting to MongoDB:', error.message);
+    throw new Error('Error connecting to MongoDB');
   }
 };
 
