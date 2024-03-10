ba1942cc71d00defdb80f079113ecd38755bc140
Champ <droped17@gmail.com>
2024-03-10 13:45:31 +0700

    refactor: change path in blog page

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -14,7 +14,7 @@ import Image from "next/image";
 const getPostById = async (params: any) => {
   try {
     const res = await axios.get(
-      `/api/postById/?postId=${params}`
+      `https://demo-next-jk36.vercel.app/api/postById/?postId=${params}`
     );
     return res.data.foundPost;
   } catch (error) {
